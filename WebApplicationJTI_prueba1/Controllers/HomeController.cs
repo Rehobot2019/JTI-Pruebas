60e68617404218a9ae6c622cf8530327ed518a71
jaherrada <43144061+jaherrada@users.noreply.github.com>
2019-01-09 00:14:17 -0500

    ok todo

---
+++ WebApplicationJTI_prueba1/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplicationJTI_prueba1.Models;
+
+namespace WebApplicationJTI_prueba1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+   
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Sistema de Geolocalización Vehículos JTI.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Detalles de Contacto.";
+
+            return View();
+        }
+
+        public IActionResult Reportes()
+        {
+            ViewData["Message"] = "Reportes de Geolocalización.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult PruebaJson()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplicationJTI_prueba1/Models/Coordenadas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationJTI_prueba1.Models
+{
+    public class Coordenadas
+    {
+        public int CoordenadasID { get; set; }
+        [Required]
+        public float Latitud { get; set; }
+        [Required]
+        public float Longitud { get; set; }
+        [Required]
+        public DateTime FechaHora { get; set; }
+    }
+}
